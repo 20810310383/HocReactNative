192c4a666546679f85822b1ec5bb592a6b3836c3
Do Khac Tu <dokhactu1822@gmail.com>
2024-11-29 14:14:50 +0700

    commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState<string>("a")
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <TextInput 
+        onChangeText={v => setName(v)}
+        value={name}
+        autoCapitalize='words'
+        // keyboardType='numeric'
+        // maxLength={6}
+        // multiline={true}
+        autoCorrect={false}
+        style={styles.textInput}
+         />
+        <Text style={styles.text}>{name}</Text>
+      </View>
+
+      <Button title="Add new"  />
+
+      <Text style={styles.text}>
+        hello khac tu hihi
+        <Text style={styles.tudeptrai}> tu dep trai</Text>
+        </Text>      
     </View>
   );
 }
@@ -14,7 +36,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',    
+    paddingTop: 50,
+    paddingHorizontal: 20
   },
+
+  text: {
+    fontSize: 30,
+    color: "red"
+  },
+  tudeptrai: {
+    color: "green"
+  }, 
+  textInput: {
+    borderColor: "violet",
+    borderWidth: 1,
+    padding: 10
+  }
 });
